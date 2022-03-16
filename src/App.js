7230b8d7490208b6db8a5b114a8cfd0481515130
amirabenz <amira.benzid09@gmail.com>
2022-03-16 11:30:28 +0100

    added a container around mt code to make sure bootstrap works perfectly

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        © Coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/amira-ben-zid-9b9451223/"
-          target="_blank noreferrer"
-        >
-          Amira Benzid
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://github.com/amirabenz/react-weather-app"
-          target="_blank noreferrer"
-        >
-          opened-sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          © Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/amira-ben-zid-9b9451223/"
+            target="_blank noreferrer"
+          >
+            Amira Benzid
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/amirabenz/react-weather-app"
+            target="_blank noreferrer"
+          >
+            opened-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
