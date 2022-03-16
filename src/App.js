c428930aba8c679990669868193dc424e94f28b8
amirabenz <amira.benzid09@gmail.com>
2022-03-16 10:54:55 +0100

    added a footer with my linkedin profile

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        © Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/amira-ben-zid-9b9451223/"
+          target="_blank noreferrer"
+        >
+          Amira Benzid
+        </a>
+      </footer>
+    </div>
+  );
 }
